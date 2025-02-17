f1d1ca627506631cc3cbd09134527fc849f7996f
luong-tran <nta4420@gmail.com>
2025-02-17 15:53:28 +0700

    init project

---
+++ ios-config-cicd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-config-cicd
+//
+//  Created by luong.tran on 17/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
